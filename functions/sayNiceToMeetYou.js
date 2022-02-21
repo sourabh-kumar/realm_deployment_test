c9b8853978d12dda6f908be58ab1a1edd8a9c0f7
soukumar3 <91651229+soukumar3@users.noreply.github.com>
2022-02-21 13:40:13 +0530

    testing 2 functions deplpoyment

--- functions/sayNiceToMeetYou.js
+++ functions/sayNiceToMeetYou.js
@@ -1,36 +1,36 @@
 // This function is the endpoint's request handler.
-exports = function(incoming, response) {
+exports = function (incoming, response) {
   //{ query, headers, body}
-    // Data can be extracted from the request as follows:
+  // Data can be extracted from the request as follows:
 
-    // Query params, e.g. '?arg1=hello&arg2=world' => {arg1: "hello", arg2: "world"}
-    // const {arg1, arg2} = query;
+  // Query params, e.g. '?arg1=hello&arg2=world' => {arg1: "hello", arg2: "world"}
+  // const {arg1, arg2} = query;
 
-    // Headers, e.g. {"Content-Type": ["application/json"]}
-    // const contentTypes = headers["Content-Type"];
+  // Headers, e.g. {"Content-Type": ["application/json"]}
+  // const contentTypes = headers["Content-Type"];
 
-    // Raw request body (if the client sent one).
-    // This is a binary object that can be accessed as a string using .text()
-    // const reqBody = body;
+  // Raw request body (if the client sent one).
+  // This is a binary object that can be accessed as a string using .text()
+  // const reqBody = body;
 
-    // console.log("arg1, arg2: ", arg1, arg2);
-    // console.log("Content-Type:", JSON.stringify(contentTypes));
-    // console.log("Request body:", reqBody);
+  // console.log("arg1, arg2: ", arg1, arg2);
+  // console.log("Content-Type:", JSON.stringify(contentTypes));
+  // console.log("Request body:", reqBody);
 
-    // You can use 'context' to interact with other Realm features.
-    // Accessing a value:
-    // var x = context.values.get("value_name");
+  // You can use 'context' to interact with other Realm features.
+  // Accessing a value:
+  // var x = context.values.get("value_name");
 
-    // Querying a mongodb service:
-    // const doc = context.services.get("mongodb-atlas").db("dbname").collection("coll_name").findOne();
+  // Querying a mongodb service:
+  // const doc = context.services.get("mongodb-atlas").db("dbname").collection("coll_name").findOne();
 
-    // Calling a function:
-    // const result = context.functions.execute("function_name", arg1, arg2);
+  // Calling a function:
+  // const result = context.functions.execute("function_name", arg1, arg2);
 
-    // The return value of the function is sent as the response back to the client
-    // when the "Respond with Result" setting is set.
-    console.log("FROM niceToMeetYou");
-    console.log("GOING TO SAY GOOD BYE");
-    context.functions.execute("sayGoodBye");
-    return  "Nice to meet you";
+  // The return value of the function is sent as the response back to the client
+  // when the "Respond with Result" setting is set.
+  console.log("FROM niceToMeetYou");
+  console.log("GOING TO SAY GOOD BYE");
+  context.functions.execute("sayGoodBye");
+  return "Say nice to meet you function has completed execution";
 };
